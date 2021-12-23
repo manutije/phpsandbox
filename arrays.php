0d1d3d0d7a44f6b427f7411842a9be3a96a746a1
manutije <manu.tijerina@gmail.com>
2021-12-23 20:36:05 +0000

    Arrays php created

---
+++ arrays.php
@@ -0,0 +1,16 @@
+<?php
+
+	//Array - Variable that holds multiple values
+	/*
+		-Indexed
+		-Associative
+		-Multi-dimensional
+	*/
+
+	//Indexed
+	$people =  array('Gonzalo', 'Manuel', 'Tomas');
+	$ids =[11,22,33];
+	//echo $ids[2];
+	$ids[] = 44;
+	print_r($ids);
+?>
