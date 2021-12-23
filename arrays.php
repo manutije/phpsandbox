054ba16c71ed03553307d60cad7aa2f25a829f5a
manutije <48832967+manutije@users.noreply.github.com>
2021-12-23 16:02:20 -0600

    combine multiple arrays

--- arrays.php
+++ arrays.php
@@ -13,7 +13,7 @@
 	$ids =[11,22,33];
 	// You can use echo to print an element of an array
 	echo "<h3> Printing the value of people array with index 1</h3>";
-	echo $people[2];
+	echo $people[1];
 
 	//You can add an element to an array by using blank brackets
 	$ids[] = 44;
@@ -30,6 +30,13 @@
 	//In the example above, Manuel is the key, and 23 is the value.
 	echo "<h3> printing the value of key Manuel</h3>";
 	echo $people['Manuel']
+
+	//Array of IDS
+	$ids = [11=>'Manuel',22=>'Tomas',33=>'Maria'];
+
+	//Combine both IDs
+	echo "<h3> Combine both Associative Arrays </h3>";
+	echo $people[$ids[33]];
 
 
 ?>
