b54d4db14c860f1a7e0772378f57190fda506b96
manutije <48832967+manutije@users.noreply.github.com>
2021-12-23 16:17:00 -0600

    First part of multi-dimensional

--- arrays.php
+++ arrays.php
@@ -9,6 +9,7 @@
 	//=================================================================
 	//Indexed
 
+	echo "<h2>Indexed Arrays</h2>";
 	$people =  array('Gonzalo', 'Manuel', 'Tomas');
 	$ids =[11,22,33];
 	// You can use echo to print an element of an array
@@ -23,8 +24,9 @@
 	print_r($ids);
 
 	//=================================================================
-	
 	//Associative Arrays
+
+	echo "<h2>Associative Arrays</h2>";
 	$people = array('Manuel' => 23, 'Tomas' =>22, 'Maria'=>19);
 	
 	//In the example above, Manuel is the key, and 23 is the value.
@@ -44,6 +46,22 @@
 	//Echo all elements of the people array
 	echo "<h3> All the elements of \$people array </h3>";
 	print_r($people);
+
+	//=================================================================
+	//Multi-dimensional Arrays
+
+	echo "<h2>Multi-Dimensional Arrays</h2>";
+	$cars = array(
+		array('toyota',20,10),
+		array('honda',10,3),
+		array('nissan',40,23)
+	);
+	echo"<h3>Printing the name of the brand</h3>";
+	echo $cars[1][0];
+	echo"<h3>Printing the number of stock</h3>";
+	echo $cars[1][1];
+	echo"<h3>Printing the number of sold cars</h3>";
+	echo $cars[1][2];
 
 
 ?>
