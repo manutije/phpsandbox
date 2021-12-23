248b9f115591df282d0e9c089b3c345f218c2c78
manutije <48832967+manutije@users.noreply.github.com>
2021-12-23 15:35:22 -0600

    Clear comments on arrays.php

--- arrays.php
+++ arrays.php
@@ -1,5 +1,4 @@
 <?php
-
 	//Array - Variable that holds multiple values
 	/*
 		-Indexed
@@ -7,10 +6,22 @@
 		-Multi-dimensional
 	*/
 
+	//=================================================================
 	//Indexed
+
 	$people =  array('Gonzalo', 'Manuel', 'Tomas');
 	$ids =[11,22,33];
+	// You can use echo to print an element of an array
 	//echo $ids[2];
+
+	//You can add an element to an array by using blank brackets
 	$ids[] = 44;
+
+	//With this funciton, you can print all the elements of the array
 	print_r($ids);
+
+	//=================================================================
+	//Associative
+
+	
 ?>
