1a4ec472186327c442eeea359f120652631b05ba
manutije <48832967+manutije@users.noreply.github.com>
2021-12-24 12:15:05 -0600

    Created functions folder

---
+++ functions/functions.php
@@ -0,0 +1,15 @@
+<?php
+    #Function: Block of code that can be repeatedly called
+
+    /*
+        Camel Case - myFunction()
+        Lower Case - my_function()
+        Pascal Case - MyFunction() Usually for classes
+    */
+
+    function simpleFunction(){
+        echo 'Hello World';
+    }
+
+    simpleFunction();
+?>
