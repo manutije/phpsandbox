b88db21fa3213923d3317b5edaf58b4147beb685
manutije <48832967+manutije@users.noreply.github.com>
2021-12-24 13:35:29 -0600

    date link fixed

--- dates/index.php
+++ dates/index.php
@@ -13,7 +13,7 @@
     </div>
 
     <div>
-        <?php include 'dates.php';?>
+        <?php include 'date.php';?>
     </div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
