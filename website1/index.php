cd538046997288c58ac0b260900f302e5696edc7
manutije <48832967+manutije@users.noreply.github.com>
2021-12-24 16:13:11 -0600

    modify foreach

--- website1/index.php
+++ website1/index.php
@@ -14,6 +14,13 @@
     <div class="container">
         <h1>Server & File Info</h1>
         <?php if($server): ?>
+            <ul class="list-group">
+               <?php foreach ($server as $key => $value){
+                   echo "<li class="list-group-item">";
+                   echo "<strong>$key:</strong>";
+                   echo " $value </li>";
+               } ?>
+            </ul>
         <?php endif; ?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
