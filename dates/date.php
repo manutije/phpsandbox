cc15b28277111d6c7c8800514e72e37f18c90e7d
manutije <48832967+manutije@users.noreply.github.com>
2021-12-24 13:31:30 -0600

    corrected headings

--- dates/date.php
+++ dates/date.php
@@ -38,24 +38,24 @@
 
     //Create a timestamp (Horas,minutos,segundos, mes, dia, anio)
     $timestamp = mktime(12,5,0,4,21,1998);
-    echo "<h5> Impresion TimeStamp 1</h5>";
+    echo "<h5> Impresion TimeStamp 1 (Fecha de cumpleanos)</h5>";
     echo date('d/m/Y h:i:s a',$timestamp);
 
     //Create a timestamp from a string
     $timestamp2 =strtotime('3:00am December 23 2021');
-    echo "<h5> Impresion TimeStamp 2</h5>";
+    echo "<h5> Impresion TimeStamp 2 (3 am de 23/12/2021)</h5>";
     echo date('d/m/Y h:i:s a',$timestamp2);
 
     $timestamp3 =strtotime('tomorrow');
-    echo "<h5> Impresion TimeStamp 3</h5>";
+    echo "<h5> Impresion TimeStamp 3 (manana)</h5>";
     echo date('d/m/Y h:i:s a',$timestamp3);
 
     $timestamp4 =strtotime('next tuesday');
-    echo "<h5> Impresion TimeStamp 4</h5>";
+    echo "<h5> Impresion TimeStamp 4 (Proximo martes)</h5>";
     echo date('d/m/Y h:i:s a',$timestamp3);
 
     $timestamp5 =strtotime('+2 Months');
-    echo "<h5> Impresion TimeStamp 5</h5>";
+    echo "<h5> Impresion TimeStamp 5 (En dos meses)</h5>";
     echo date('d/m/Y h:i:s a',$timestamp3);
 
     //=================================================================
