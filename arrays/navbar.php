aac84db300870f87b40c801c72e13c7bd1663240
manutije <48832967+manutije@users.noreply.github.com>
2021-12-24 11:33:35 -0600

    change Navbar

--- arrays/navbar.php
+++ arrays/navbar.php
@@ -4,9 +4,7 @@ echo '
     <div class="nav-wrapper blue">
       <a href="#" class="brand-logo center">Arrays</a>
       <ul id="nav-mobile" class="left hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
+        <li><a href="../">Menu</a></li>
       </ul>
     </div>
   </nav>
