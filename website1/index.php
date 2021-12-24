1e867428e1825546c03bc6b91876c74c224f21a9
manutije <48832967+manutije@users.noreply.github.com>
2021-12-24 16:06:44 -0600

    added error display

--- website1/index.php
+++ website1/index.php
@@ -1,4 +1,9 @@
-<?php include './server_info.php';?>
+<?php 
+    include './server_info.php';
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
