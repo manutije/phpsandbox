21bf98cdcd05abf773222ddfe67388919e77e909
manutije <48832967+manutije@users.noreply.github.com>
2021-12-24 11:35:49 -0600

    create loop folder

---
+++ loops/navbar.php
@@ -0,0 +1,12 @@
+<?php
+echo '
+<nav>
+    <div class="nav-wrapper lime accent-1">
+      <a href="#" class="brand-logo center">Loops</a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li><a href="../">Menu</a></li>
+      </ul>
+    </div>
+  </nav>
+';
+?>
