668622262853e1e2f5e07ee0b272a4ebd44e3e5e
manutije <48832967+manutije@users.noreply.github.com>
2021-12-24 13:34:46 -0600

    full webpage for dates

---
+++ dates/navbar.php
@@ -0,0 +1,12 @@
+<?php
+echo '
+<nav>
+    <div class="nav-wrapper grey darken-3">
+      <a href="#" class="brand-logo center">Dates</a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li><a href="../">Menu</a></li>
+      </ul>
+    </div>
+  </nav>
+';
+?>
