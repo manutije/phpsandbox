044a902db20173a48fd53e242f2f4d9fb2f3320b
manutije <48832967+manutije@users.noreply.github.com>
2021-12-24 16:30:48 -0600

    added hr at the bootom

--- website1/index.php
+++ website1/index.php
@@ -21,7 +21,9 @@
                 <?php endforeach; ?>
             </ul>
         <?php endif; ?>
+        <br>
         <hr>
+        <br>
         <h1>Client Info</h1>
         <?php if($client): ?>
             <ul class="list-group">
@@ -33,6 +35,9 @@
                 <?php endforeach; ?>
             </ul>
         <?php endif; ?>
+        <hr>
+        <br>
+        <br>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
