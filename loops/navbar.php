478f07cb830508aa3857ab52af097c92890b2c26
manutije <48832967+manutije@users.noreply.github.com>
2021-12-24 11:37:03 -0600

    Change color

--- loops/navbar.php
+++ loops/navbar.php
@@ -1,7 +1,7 @@
 <?php
 echo '
 <nav>
-    <div class="nav-wrapper lime accent-1">
+    <div class="nav-wrapper amber darken-4">
       <a href="#" class="brand-logo center">Loops</a>
       <ul id="nav-mobile" class="left hide-on-med-and-down">
         <li><a href="../">Menu</a></li>
