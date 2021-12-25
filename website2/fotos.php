e37852b1a7046f11bc032701b6b7b4071d6f8cf8
manutije <48832967+manutije@users.noreply.github.com>
2021-12-25 12:04:42 -0600

    remove echos

--- website2/fotos.php
+++ website2/fotos.php
@@ -19,24 +19,15 @@
     //Tamano de variable files
     $size = sizeof($files)-2;
 
-
-    echo $size;
-    echo "<hr>";
-    
     while($contador_prim < $size){
         while($contador_terc < 9 and $contador_prim < $size){
             $temp[$contador_terc]= $files[$contador_prim];
-            echo "Folder: $contador_secu | Picture: $contador_prim";
-            echo "<br>";
             $contador_prim = $contador_prim +1;
             $contador_terc = $contador_terc +1;
         }
         $pages_file[$contador_secu]=$temp;
-        echo "<hr>";
         $contador_secu = $contador_secu +1;
         $contador_terc = 0;
         $temp = array();
-    }
-    print_r($pages_file);
-    
+    }  
 ?>
