da4c0a75fa850c0bf0085c55a7c5ac9b1cd27b4c
manutije <48832967+manutije@users.noreply.github.com>
2021-12-24 23:44:09 -0600

    Eliminated dot directories

--- website2/fotos.php
+++ website2/fotos.php
@@ -1,8 +1,4 @@
 <?php
-$dir    = '/var/www';
-$files1 = scandir($dir);
-$files2 = scandir($dir, 1);
-
-print_r($files1);
-print_r($files2);
+    $directory   = '/var/www';
+    $scanned_directory = array_diff(scandir($directory), array('..', '.'));
 ?>
