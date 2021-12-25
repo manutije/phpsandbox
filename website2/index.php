e5b393011e967362db161a439924ee04158e87e1
manutije <48832967+manutije@users.noreply.github.com>
2021-12-25 12:37:56 -0600

    automatic buttons

--- website2/index.php
+++ website2/index.php
@@ -31,10 +31,9 @@
         <br>
         <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
             <div class="btn-group me-2" role="group" aria-label="First group">
-                <button type="button" class="btn btn-primary">1</button>
-                <button type="button" class="btn btn-primary">2</button>
-                <button type="button" class="btn btn-primary">3</button>
-                <button type="button" class="btn btn-primary">4</button>
+                <?php foreach($page_file as $key=> $value): ?>
+                    <button type="button" class="btn btn-primary">$key +1</button>
+                <?php endforeach; ?>
             </div>
         </div>
     </div>
