e631d2f73b5e672f59a2481551db7d1c5a0b429a
manutije <48832967+manutije@users.noreply.github.com>
2021-12-25 11:38:21 -0600

    First try divide files in 10

--- website2/fotos.php
+++ website2/fotos.php
@@ -1,7 +1,31 @@
 <?php
+    // Read al files and save them in files variable
     $directory   = './uploads';
     $files = array_diff(scandir($directory), array('..', '.'));
-    $contador = 0;
+
+    //Contador primario de 0 a 128
+    $contador_prim = 0;
+
+    //Contador secundario de 0 a 9
+    $contador_secu = 0;
+
+    //Arreglo de paginas y temporal
+    $pages_file = [];
+    $temp = [];
+
+    //Tamano de variable files
     $size = sizeof($files);
+
+
     echo $size;
+    while ($contador_prim < $size){
+        while($contador_prim < 10){
+            $temp[]= $files[$contador_prim];
+            $contador_prim ++;
+        }
+        $pages_file[$contador_secu]=$temp
+        $contador_secu ++;
+        $temp = array();
+    }
+    print_r($pages_file);
 ?>
