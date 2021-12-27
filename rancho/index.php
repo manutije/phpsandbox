474781857798545eeb73dd26bba4817464548bc9
manutije <manu.tijerina@gmail.com>
2021-12-27 16:02:41 -0600

    added navbar

--- rancho/index.php
+++ rancho/index.php
@@ -10,6 +10,20 @@
 <body>
     <div class="container">
         <h1 class="text-center">Administracion Rancho</h1>
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Caballos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Vacas Bravas</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Toros Bravos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Ganado Manso</a>
+            </li>
+        </ul>
     </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
