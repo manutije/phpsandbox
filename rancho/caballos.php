4e7a07e6d0a4a07a3dfe2623cd0b3b7f68c90ddd
manutije <manu.tijerina@gmail.com>
2021-12-27 16:18:34 -0600

    Caballos created

---
+++ rancho/caballos.php
@@ -0,0 +1,13 @@
+<table>
+    <tr>
+        <th>Nombre</th>
+        <th>Owner</th>
+        <th>Color</th>
+    </tr>
+    <tr>
+        <td>Rabanita</td>
+        <td>Manuel Tijerina</td>
+        <td>Cafe</td>
+    </tr>
+
+</table>
