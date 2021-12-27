834ffd2de3b835f571dc72478915823d784fc13e
manutije <manu.tijerina@gmail.com>
2021-12-27 15:59:22 -0600

    Center text and addedd container

--- rancho/index.php
+++ rancho/index.php
@@ -8,7 +8,9 @@
     <title>Rancho</title>
 </head>
 <body>
-    <h1>Administracion Rancho</h1>
+    <div class="container">
+        <h1 class="text-center">Administracion Rancho</h1>
+    </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
