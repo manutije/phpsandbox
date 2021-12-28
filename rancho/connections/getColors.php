33079e333888d8835da0d392289c6716fb5d280e
manutije <manu.tijerina@gmail.com>
2021-12-27 20:59:13 -0600

    added key

--- rancho/connections/getColors.php
+++ rancho/connections/getColors.php
@@ -18,7 +18,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    $Colores[]=$row["ColorName"];
+    $Colores[]=$row["ID"] => $row["ColorName"];
   }
 } else {
     $Colores[]="NoData";
