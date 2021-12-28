8b469e2569f49b3a0e515d49b39cd9a90f247327
manutije <manu.tijerina@gmail.com>
2021-12-28 00:16:51 -0600

    Try full function

--- rancho/connections/setHorse.php
+++ rancho/connections/setHorse.php
@@ -8,29 +8,19 @@
     $sex = $_GET["sex"];
     $color = $_GET["color"];
 
-    $sql = "INSERT INTO Caballos (Name,Owner,Color,Sex)
+    $sql = "INSERT INTO Caballos (Name,Owner,Sex,Color)
     VALUES ('".
     $hname."',".
     $owner.",'".
     $sex."',".
     $color.")";
-    echo $sql;
 
-    /*
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
     // Check connection
     if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
     }
-
-    $sql = "INSERT INTO Caballos (Name,Owner,Color,Sex)
-    VALUES (".
-    $name.",".
-    $owner.",".
-    $sex.",".
-    $color.")";
-    echo $sql;
     
     if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
