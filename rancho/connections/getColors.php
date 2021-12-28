4f4641b72919d9eea70c4c646b22fb65afbd7d31
manutije <manu.tijerina@gmail.com>
2021-12-27 21:06:00 -0600

    Why wont work

--- rancho/connections/getColors.php
+++ rancho/connections/getColors.php
@@ -1,28 +1,29 @@
 <?php
-$servername = "localhost";
-$username = "adminer";
-$password = "@Avanti2";
-$dbname = "Rancho";
+  $servername = "localhost";
+  $username = "adminer";
+  $password = "@Avanti2";
+  $dbname = "Rancho";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-$Colores = [];
-
-$sql = "SELECT * FROM Colors";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    $Colores[]=$row["ID"] => $row["ColorName"];
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
   }
-} else {
-    $Colores[]="NoData";
-}
-$conn->close();
-print_r($Colores);
+  $Colores = [];
+
+  $sql = "SELECT * FROM Colors";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      $Colores[]=$row["ID"] => $row["ColorName"];
+      echo "$row['ID']: $row['ColorName']";
+    }
+  } else {
+      $Colores[]="NoData";
+  }
+  $conn->close();
+  print_r($Colores);
 ?>
