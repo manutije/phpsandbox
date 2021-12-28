f35f3187ab6f55f99b6e14e61ee5e6b09d74ae10
manutije <manu.tijerina@gmail.com>
2021-12-27 22:11:27 -0600

    Added First Name

--- rancho/connections/getOwner.php
+++ rancho/connections/getOwner.php
@@ -17,7 +17,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "<option value='".$row["ID"]."'>".$row["firstName"]." ".$row["LastName"]."</option>";
+    echo "<option value='".$row["ID"]."'>".$row["FirstName"]." ".$row["LastName"]."</option>";
   }
 } else {
   echo "0 results";
