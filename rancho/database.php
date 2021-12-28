7e9408610ba9bc6d2c7ef29f5ee67bac7575734c
manutije <manu.tijerina@gmail.com>
2021-12-27 19:01:32 -0600

    try get data from MySQL

--- rancho/database.php
+++ rancho/database.php
@@ -2,13 +2,25 @@
 $servername = "localhost";
 $username = "adminer";
 $password = "@Avanti2";
+$dbname = "Rancho";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+$sql = "SELECT Name,ColorName,FirstName from Caballos INNER JOIN Colors on Colors.ID = Caballos.Color INNER JOIN Owners on Owners.ID = Caballos.Owner";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "Name: " . $row["Name"]. " - Color: " . $row["ColorName"]. " " . $row["FirstName"]. "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
+  $conn->close();
 ?>
