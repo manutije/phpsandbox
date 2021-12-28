ddaa0d28a4cafd15daae6f89d558a0997b51eea1
manutije <manu.tijerina@gmail.com>
2021-12-27 21:23:32 -0600

    modified getColors

--- rancho/connections/getColors.php
+++ rancho/connections/getColors.php
@@ -1,40 +1,26 @@
 <?php
-  $servername = "localhost";
-  $username = "adminer";
-  $password = "@Avanti2";
-  $dbname = "Rancho";
+$servername = "localhost";
+$username = "adminer";
+$password = "@Avanti2";
+$dbname = "Rancho";
 
-  // Create connection
-  $conn = new mysqli($servername, $username, $password, $dbname);
-
-  // Check connection
-  if ($conn->connect_error) {
-    echo "Connection failed";
-    die("Connection failed: " . $conn->connect_error);
-  }else{
-    //Empty Array
-    $Colores = [];
-
-    //Query
-    $sql = "SELECT ID, ColorName FROM Colors";
-
-    //Execute Query
-    $result = $conn->query($sql);
-
-    
-    if ($result->num_rows > 0) {
-      // output data of each row
-      while($row = $result->fetch_assoc()) {
-        echo "$row['ID']: $row['ColorName']";
-        /*
-        $Colores[]=$row["ID"] => $row["ColorName"];
-        */
-      }
-    } else {
-        $Colores[]="NoData";
-    }
-    echo "This work";
-    $conn->close();
-    print_r($Colores);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
+
+$sql = "SELECT * FROM Colors";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["ID"]. " - Color: " . $row["ColorName"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
 ?>
