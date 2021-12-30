2282a8c1a9cde4bd11b273842a08ebd501b03cf1
manutije <manu.tijerina@gmail.com>
2021-12-29 21:08:41 -0600

    Corrected directory

--- rancho/elements/navbar2.php
+++ rancho/elements/navbar2.php
@@ -1,8 +1,8 @@
 <ul class="nav justify-content-center">
     <li class="nav-item">
-        <a class="nav-link" href="./index.php">Inicio</a>
+        <a class="nav-link" href="../index.php">Inicio</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="./index.php?page=caballos">Caballoss</a>
+        <a class="nav-link" href="../index.php?page=caballos">Caballos</a>
     </li>
 </ul>
