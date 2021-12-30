d78e492db3ed01418de4f1963a20ab02a0bf743e
manutije <manu.tijerina@gmail.com>
2021-12-29 21:37:02 -0600

    try this way

--- rancho/connections/getHorse.php
+++ rancho/connections/getHorse.php
@@ -11,7 +11,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$caballo ="No working";
 $sql = "SELECT Caballos.ID, Name,ColorName,FirstName, LastName,Sex from Caballos INNER JOIN Colors on Colors.ID = Caballos.Color INNER JOIN Owners on Owners.ID = Caballos.Owner WHERE Caballos.ID = 2";
 $result = $conn->query($sql);
 echo $result->num_rows;
@@ -19,14 +18,13 @@ if ($result->num_rows > 0) {
   // output data of each row
   $row = $result->fetch_assoc();
   echo $row["Name"];
-  /*
-  $caballo = [
+  
+  $caballo = array(
       $row["Name"],
       $row["FirstName"].' '.$row["LastName"],
       $row["Sex"],
-      $row["ColorName"],
-      '<a class="btn btn-warning" href="./pages/horse.php?id='.$row["ID"].'">Complete View</a>'
-  ];
+      $row["ColorName"]
+  );
   */
 } else {
   $caballo=["No connection","No connection","No connection","No connection"];
