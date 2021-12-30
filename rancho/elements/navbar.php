1361e2a0743173373d72239875893415d0ae0eda
manutije <manu.tijerina@gmail.com>
2021-12-29 20:00:00 -0600

    Add buttons to btn

--- rancho/elements/navbar.php
+++ rancho/elements/navbar.php
@@ -1,14 +1,14 @@
 <ul class="nav justify-content-center">
     <li class="nav-item">
-        <a class="nav-link" href="./index.php?page=caballos">Caballos</a>
+        <a class="btn btn-info" href="./index.php?page=caballos">Caballos</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="./index.php?page=vacasb">Vacas Bravas</a>
+        <a class="btn btn-info" href="./index.php?page=vacasb">Vacas Bravas</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="./index.php?page=torosb">Toros Bravos</a>
+        <a class="btn btn-info" href="./index.php?page=torosb">Toros Bravos</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="./index.php?page=manso">Ganado Manso</a>
+        <a class="btn btn-info" href="./index.php?page=manso">Ganado Manso</a>
     </li>
 </ul>
