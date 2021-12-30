ceac1cd44a12ea9f4f9fa035d3dd1456fcdfe176
manutije <manu.tijerina@gmail.com>
2021-12-30 12:32:29 -0600

    get info on horse

--- rancho/connections/getHorse.php
+++ rancho/connections/getHorse.php
@@ -11,7 +11,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$sql = "SELECT Caballos.ID, Name,ColorName,FirstName, LastName,Sex from Caballos INNER JOIN Colors on Colors.ID = Caballos.Color INNER JOIN Owners on Owners.ID = Caballos.Owner WHERE Caballos.ID = 2";
+$sql = "SELECT Caballos.ID, Name,ColorName,FirstName, LastName,Sex from Caballos INNER JOIN Colors on Colors.ID = Caballos.Color INNER JOIN Owners on Owners.ID = Caballos.Owner WHERE Caballos.ID =".$_GET["id"];
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
