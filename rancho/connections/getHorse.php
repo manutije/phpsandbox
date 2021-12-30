387b64eed5d4e157d36c967b632b033f9ccbb255
manutije <manu.tijerina@gmail.com>
2021-12-29 21:44:46 -0600

    add quotes

--- rancho/connections/getHorse.php
+++ rancho/connections/getHorse.php
@@ -17,7 +17,7 @@ echo $result->num_rows;
 if ($result->num_rows > 0) {
   // output data of each row
   $row = $result->fetch_assoc();
-  print_r $row;
+  print_r ($row);
   //$caballo = array($row["Name"], $row["FirstName"].' '.$row["LastName"], $row["Sex"], $row["ColorName"]);
   */
 } else {
