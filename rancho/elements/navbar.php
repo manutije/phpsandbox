03b447dbc4db1a139155bed7de777bf4512a65cf
manutije <manu.tijerina@gmail.com>
2021-12-29 20:06:27 -0600

    REmove to previous version

--- rancho/elements/navbar.php
+++ rancho/elements/navbar.php
@@ -1,14 +1,14 @@
-<ul class="btn-group justify-content-center">
-    <li>
-        <a class="btn btn-info" href="./index.php?page=caballos">Caballos</a>
+<ul class="nav justify-content-center">
+    <li class="nav-item">
+        <a class="nav-link" href="./index.php?page=caballos">Caballos</a>
     </li>
-    <li>
-        <a class="btn btn-info" href="./index.php?page=vacasb">Vacas Bravas</a>
+    <li class="nav-item">
+        <a class="nav-link" href="./index.php?page=vacasb">Vacas Bravas</a>
     </li>
-    <li>
-        <a class="btn btn-info" href="./index.php?page=torosb">Toros Bravos</a>
+    <li class="nav-item">
+        <a class="nav-link" href="./index.php?page=torosb">Toros Bravos</a>
     </li>
-    <li>
-        <a class="btn btn-info" href="./index.php?page=manso">Ganado Manso</a>
+    <li class="nav-item">
+        <a class="nav-link" href="./index.php?page=manso">Ganado Manso</a>
     </li>
 </ul>
