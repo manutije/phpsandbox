21a8ae98a55fdaa08bb057cf51dfa88c379a07d2
manutije <manu.tijerina@gmail.com>
2021-12-29 20:04:50 -0600

    remove navs

--- rancho/elements/navbar.php
+++ rancho/elements/navbar.php
@@ -1,14 +1,14 @@
 <ul class="nav justify-content-center">
     <li>
-        <a class="nav nav-link btn btn-info" href="./index.php?page=caballos">Caballos</a>
+        <a class="btn btn-info" href="./index.php?page=caballos">Caballos</a>
     </li>
     <li>
-        <a class="nav nav-link btn btn-info" href="./index.php?page=vacasb">Vacas Bravas</a>
+        <a class="btn btn-info" href="./index.php?page=vacasb">Vacas Bravas</a>
     </li>
     <li>
-        <a class="nav nav-link btn btn-info" href="./index.php?page=torosb">Toros Bravos</a>
+        <a class="btn btn-info" href="./index.php?page=torosb">Toros Bravos</a>
     </li>
     <li>
-        <a class="nav nav-link btn btn-info" href="./index.php?page=manso">Ganado Manso</a>
+        <a class="btn btn-info" href="./index.php?page=manso">Ganado Manso</a>
     </li>
 </ul>
