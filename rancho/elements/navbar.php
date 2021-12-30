1c62451761a8d3eb18d5a264f5fb7b6b41d8c874
manutije <manu.tijerina@gmail.com>
2021-12-29 20:03:19 -0600

    remove nav item

--- rancho/elements/navbar.php
+++ rancho/elements/navbar.php
@@ -1,14 +1,14 @@
 <ul class="nav justify-content-center">
-    <li class="nav-item">
+    <li>
         <a class="nav nav-link btn btn-info" href="./index.php?page=caballos">Caballos</a>
     </li>
-    <li class="nav-item">
+    <li>
         <a class="nav nav-link btn btn-info" href="./index.php?page=vacasb">Vacas Bravas</a>
     </li>
-    <li class="nav-item">
+    <li>
         <a class="nav nav-link btn btn-info" href="./index.php?page=torosb">Toros Bravos</a>
     </li>
-    <li class="nav-item">
+    <li>
         <a class="nav nav-link btn btn-info" href="./index.php?page=manso">Ganado Manso</a>
     </li>
 </ul>
