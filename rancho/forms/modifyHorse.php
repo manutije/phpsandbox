9ba9cf38ffad535859a348fc8d5cbd8abb67d673
manutije <manu.tijerina@gmail.com>
2021-12-30 01:08:22 -0600

    Try to add default name

--- rancho/forms/modifyHorse.php
+++ rancho/forms/modifyHorse.php
@@ -4,13 +4,13 @@
     <form action="./connections/setHorse.php" class="row g-3">
         <div class="col-md-3">
             <label for="hname" class="form-label">Horse name:</label>
-            <input type="text" id="hname" name="hname" value="Gabbana" class="form-control">
+            <input type="text" id="hname" name="hname" value='<?php echo $row["Name"];?>' class="form-control">
         </div>
 
         <div class="col-md-3">
             <label for="owner" class="form-label">Horse Owner:</label>
             <select id="owner" name="owner" class="form-select" aria-label="Default select example">
-                <?php include "./connections/getOwner.php";?>
+                <?php include "../connections/getOwner.php";?>
             </select>
         </div>
 
@@ -25,10 +25,10 @@
         <div class="col-md-3">
             <label for="color" class="form-label">Horse color:</label>
             <select id="color" name="color" class="form-select" aria-label="Default select example">
-                <?php include "./connections/getColors.php";?>
+                <?php include "../connections/getColors.php";?>
             </select>
         </div>
 
-        <button type="submit" class="btn btn-primary">Registrar</button>
+        <button type="submit" class="btn btn-primary">Modificar</button>
     </form>
 </div>
