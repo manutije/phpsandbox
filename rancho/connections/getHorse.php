c130f0bf482d35dd2f5372fee26c12c1129cc675
manutije <manu.tijerina@gmail.com>
2021-12-29 21:52:54 -0600

    This works !!!

--- rancho/connections/getHorse.php
+++ rancho/connections/getHorse.php
@@ -13,7 +13,6 @@ if ($conn->connect_error) {
 }
 $sql = "SELECT Caballos.ID, Name,ColorName,FirstName, LastName,Sex from Caballos INNER JOIN Colors on Colors.ID = Caballos.Color INNER JOIN Owners on Owners.ID = Caballos.Owner WHERE Caballos.ID = 2";
 $result = $conn->query($sql);
-echo $result->num_rows;
 if ($result->num_rows > 0) {
   // output data of each row
   $row = $result->fetch_assoc();
