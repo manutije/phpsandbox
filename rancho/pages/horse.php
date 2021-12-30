f1c12394446ae8f1b76f19ff7a197d542b831622
manutije <manu.tijerina@gmail.com>
2021-12-29 21:04:57 -0600

    Try new page

--- rancho/pages/horse.php
+++ rancho/pages/horse.php
@@ -1,3 +1,22 @@
-<?php
-    echo "Here you will see all the information of the horse";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Info Caballo</title>
+</head>
+<body>
+    <div class="container">
+        <h1 class="text-center">Informacion de caballo</h1>
+        <?php include './elements/navbar.php';?>
+        <br>
+        <hr>
+        <div class="container justify-content-center">
+
+        </div>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
