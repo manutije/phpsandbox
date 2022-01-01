bdad38b212bfc0451bf376b8b5ed8e3e485e32de
manutije <manu.tijerina@gmail.com>
2022-01-01 00:14:58 -0600

    Center Text

--- rancho/index.php
+++ rancho/index.php
@@ -19,7 +19,7 @@
         <br>
         <hr>
         <div class="container justify-content-center">
-            <h2>Happy New Year 2022!!</h2>
+            <h2 class="text-center">Happy New Year 2022!!</h2>
         </div>
     </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
