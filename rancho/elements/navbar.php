ddbb45c335c6d3d3f814c0f78a0d5f376dcc2532
manutije <manu.tijerina@gmail.com>
2022-01-01 00:09:30 -0600

    Added Navbar and happy new Year

--- rancho/elements/navbar.php
+++ rancho/elements/navbar.php
@@ -1,5 +1,8 @@
 <ul class="nav justify-content-center">
     <li class="nav-item">
+        <a class="nav-link" href="./index.php">Inicio</a>
+    </li>
+    <li class="nav-item">
         <a class="nav-link" href="./index.php?page=caballos">Caballos</a>
     </li>
     <li class="nav-item">
